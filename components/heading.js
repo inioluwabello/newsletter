6b6f99beca1e379b90dee98c00ac8d103e1ace9a
inioluwabello <inioluwabellodavid@gmail.com>
2023-07-08 07:38:09 +0100

    Separated Newsletter

---
+++ components/heading.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Heading({ title, favicon }) {
+  return (
+    <Head>
+      <link rel="icon" href={favicon ? favicon : "/favicon.ico"} />
+      <meta name="description" content="" />
+      <meta name="og:title" content="LinkedIn:Clone" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>{title}</title>
+    </Head>
+  );
+}
---
+++ components/newsletter-sign-up/PageIllustrationMobile.js
@@ -0,0 +1,10 @@
+const PageIllustrationMobile = () => {
+  const rootAssets = "/assets/";
+  
+  return (
+    <div className="d-xs-block d-sm-block d-md-none d-lg-none d-xl-none">
+      <img style={{width: "100%"}} src={`${rootAssets}images/illustration-sign-up-mobile.svg`} />
+    </div>
+  );
+};
+export default PageIllustrationMobile;
